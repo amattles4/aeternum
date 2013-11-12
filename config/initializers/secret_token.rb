6c8ad543dd387462b539a8ea5ec122547d4dd26f
Aaron Attles <amattles4@msn.com>
2013-11-12 17:09:58 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aeternum::Application.config.secret_key_base = '02c5674deb777346dc212da2ad8bddb0cbd56ef425f233c8d47d347f2639ebc548cd8be9f916eb5f6b0c39b0bc079d3777d38d314f5922b383864d431e095229'
